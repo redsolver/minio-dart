0782a3e14fee8471adf4644fa7a80e6428725bae
redsolver <info@redsolver.net>
2022-09-16 17:26:09 +0200

    Remove Content-Type JSON header

--- lib/src/minio.dart
+++ lib/src/minio.dart
@@ -223,7 +223,6 @@ class Minio {
     MinioInvalidBucketNameError.check(bucket);
 
     final headers = <String, String>{};
-    headers['content-type'] = 'application/json';
 
     return await _client.request(
       method: 'PUT',
